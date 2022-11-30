df107503b6e19c73ef5c7eb4915f602b3eba8a8e
lorraynecardozo <lorraynecardozo15@gmail.com>
2022-11-30 16:53:11 -0300

    Creating class Conteudo

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import desafio.bootcamp.Curso;
+import desafio.bootcamp.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Curso curso = new Curso();
+        curso.setTitulo("Curso java");
+        curso.setDescricao("Descrição curso java");
+        curso.setCargaHoraria(10);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria java");
+        mentoria.setDescricao("Descrição");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso);
+        System.out.println(mentoria);
     }
 }
---
+++ src/desafio/bootcamp/Conteudo.java
@@ -0,0 +1,34 @@
+package desafio.bootcamp;
+
+public abstract class Conteudo {
+    private String titulo;
+    private String descricao;
+    protected static final double XP_PADRAO = 10d;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    @Override
+    public String toString() {
+        return "Conteudo{" +
+                "titulo='" + titulo + '\'' +
+                ", descricao='" + descricao + '\'' +
+                '}';
+    }
+
+    public abstract double calcularXP();
+
+}
